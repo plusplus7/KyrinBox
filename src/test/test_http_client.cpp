49bfcdf59d73e8766b5bbd2e25f50298da660dba
plusplus7 <JackieDeng.cn@gmail.com>
2015-01-15 14:14:48 +0800

    添加了http client的测试

---
+++ src/test/test_http_client.cpp
@@ -0,0 +1,17 @@
+#include "io/kyrin_http_client.h"
+#include <iostream>
+
+using namespace std;
+using namespace kyrin::io;
+
+void http_request_done(struct evhttp_request *req, void *arg){
+    char buf[1024];
+    int len = evbuffer_remove(req->input_buffer, &buf, sizeof(buf) - 1);
+
+    buf[len] = '\0';
+    cout<<buf<<endl;
+    event_base_loopbreak((event_base*)arg);
+}
+int main() {
+    KyrinHttpClient::make_ev_request_get(http_request_done, "127.0.0.1", 7777, "/get_status");
+}
